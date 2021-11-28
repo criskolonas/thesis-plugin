9ddbadf03f90caab00b2623ca9545e5b6b879e20
Christos Kolonas <ckolonas@csd.auth.gr>
2021-11-28 22:35:41 +0200

    making ajax request

--- scripts/load_entries.js
+++ scripts/load_entries.js
@@ -1,6 +1,17 @@
-$(document).ready(function () {
-    $("#tables_form").submit(function (e) { 
+jQuery(document).ready(function () {
+    jQuery("#table_form").submit(function (e) { 
         e.preventDefault();
+        const post_url = jQuery(this).attr("action"); //get form action url
+	    const request_method = jQuery(this).attr("method"); //get form GET/POST method
+	    const form_data = jQuery(this).serialize(); //Encode form elements for submission
         
+        jQuery.ajax({
+            type: request_method,
+            url: post_url,
+            data: form_data,
+            success: function (response) {
+                alert(form_data)
+            }
+        });
     });
 });
