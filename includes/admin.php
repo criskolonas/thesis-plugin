1a95be9842538c03c9d90ff7ace8a54e9f15cd42
Christos Kolonas <ckolonas@csd.auth.gr>
2021-11-28 21:07:18 +0200

    added ajax script

--- includes/admin.php
+++ includes/admin.php
@@ -1,4 +1,11 @@
 <?php 
+
+function enqueue_my_custom_script() {
+    wp_enqueue_script( 'load_entries_js', ABSPATH.'wp-content/plugins/thesis-plugin/scripts/load_entries.js',array('jquery'),true);
+}
+
+add_action( 'admin_enqueue_scripts', 'enqueue_my_custom_script' );
+
 add_action('admin_menu', 'thesis_plugin_menu');
  
 function thesis_plugin_menu(){
@@ -11,7 +18,7 @@ function thesis_plugin_menu(){
 function form_entries(){
     global $wpdb;
     $tables = $wpdb -> get_results("SHOW TABLES FROM local LIKE '".$wpdb->prefix."submissions_table_%'");
-    echo '<form action="">
+    echo '<form method="POST" action="" id="table_form">
     <label for="tables">Select Entries Table:</label>
     <select name="tables" id="tables">';
     foreach ($tables as $key=>$value )
